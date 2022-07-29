b6f27d55dfc4b0077b68abc0568014e3268827b7
10-Anas <anasq0q@gmail.com>
2022-07-29 23:54:12 +0300

    fix a problem

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import Home from './Home';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route,BrowserRouter,Routes } from 'react-router-dom';
 import Post from './Component/Posts/Post';
-import Header from './Component/Posts/Header';
 import Contact from './Components/Contact';
 import About from './Components/About';
 import { UilUser } from '@iconscout/react-unicons';
 import Footer from './Components/Footer';
+import Header from './Component/Header';
 
 function App() {
   return (      
