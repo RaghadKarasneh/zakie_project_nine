8e73a2a72569781db285c62acffd677465ea4761
10-Anas <anasq0q@gmail.com>
2022-08-01 23:36:28 +0300

    insert posts

--- PHP/insertPost.php
+++ PHP/insertPost.php
@@ -7,6 +7,6 @@ $image=['image'];
 $excerpt=['excerpt'];
 $body=['body'];
 
-$st= $connect->prepare("INSERT INTO posts(userId,title,image,excerpt,body) VALUES('$user_id','$title','$image','$excerpt','$body');");
+$st= $connect->prepare("INSERT INTO posts(user_id,title,body) VALUES('$user_id','$title','$body');");
 $st->execute();
 ?>
--- src/reducers/login.js
+++ src/reducers/login.js
@@ -18,7 +18,7 @@ const loginReducer=(state={error:''},action)=>{
             break;
         case "LOGIN":
                 const loadInfo = async () => {
-                    const res = await axios.get('http://localhost/project9-1/PHP/login.php?email='+state.emailORUsername+'&password='+state.password);
+                    const res = await axios.get('http://localhost/project9/PHP/login.php?email='+state.emailORUsername+'&password='+state.password);
                     sessionStorage.clear()
                 sessionStorage.setItem("user_info", res.data);
                 console.log(res.data);
